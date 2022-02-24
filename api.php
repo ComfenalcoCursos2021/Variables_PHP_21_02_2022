a804026bc3f8a36f0aa8f86ca672e9efe06150af
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-02-23 20:56:07 -0500

    Variables y tipos en PHP

---
+++ api.php
@@ -0,0 +1,22 @@
+
+<?php
+
+$nomUsu = (string) "Miguel Angel Castro Escamilla";
+$edaUsu = (int) 23;
+$altUsu = (float) 1.63;
+$HobUsu = (array) [
+    (string) "Desarrollo",
+    (string) "Musica",
+    (string) "Cocino"
+];
+$soyInt = (bool) true;
+$rutUsu = (object) [];
+$rutUsu->levantar = (int) 6;
+$vacio = null;
+$notengoDependencia = uninitialized;
+
+echo "";
+
+
+
+?>
