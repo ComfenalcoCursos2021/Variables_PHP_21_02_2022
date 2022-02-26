5f186d36d67e39a8272ef6233a9bfed2f4970fa1
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-02-25 21:36:26 -0500

    Constantes en PHP

--- api.php
+++ api.php
@@ -1,26 +1,23 @@
 
 <?php
 header('Access-Control-Allow-Origin: *');
-$nomUsu = (string) "Miguel Angel Castro Escamilla";
-$edaUsu = (int) 23;
-$altUsu = (float) 1.63;
-$HobUsu = (array) [
-    (string) "Desarrollo",
-    (string) "Musica",
-    (string) "Cocino"
-];
-$soyInt = (bool) true;
-$rutUsu = (object) [];
-$rutUsu->levantar = (int) 6;
-$vacio = null;
-// $notengoDependencia = uninitialized;
-echo $nomUsu;
-echo $edaUsu;
-echo $altUsu;
-var_dump($HobUsu);
-echo $soyInt;
-var_dump($rutUsu);
-var_dump($vacio);
+
+define("Gravedad", (float) 9.807);
+define("Gravedad2", (string) "9.807 m/s²");
+$Gravedad = (float)  9.807;
+$Gravedad2 = (string) "9.807 m/s²";
+
+echo $Gravedad;
+echo "<br>";
+echo Gravedad2;
+echo "<hr>";
+echo Gravedad;
+echo "<br>";
+echo $Gravedad2;
+
+// echo <<<FIN
+// " ' $ \\  ${!${''} = Gravedad}
+// FIN;
 
 
 
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 addEventListener("DOMContentLoaded", async(e)=>{
-    let peticion = await fetch("https://pruebacofenalco.000webhostapp.com/");
+    let peticion = await fetch("api.php");
     let texto = await peticion.text();
     document.body.insertAdjacentHTML("afterend", texto);
+    // let Gravedad = 9.807;
+    // console.log(` " ' $ \\ ${Gravedad}`);
 })
