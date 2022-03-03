33cb6f2c2fe9b6daab40683ba30368fde7bfe2f8
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-03-03 16:06:39 -0500

    Conexion al servidor 000webHosting

--- api.php
+++ api.php
@@ -1,24 +1,20 @@
-
 <?php
-header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Origin: *');
 
-define("Gravedad", (float) 9.807);
-define("Gravedad2", (string) "9.807 m/s²");
-$Gravedad = (float)  9.807;
-$Gravedad2 = (string) "9.807 m/s²";
+    define("Gravedad", (float) 9.807);
+    define("Gravedad2", (string) "9.807 m/s²");
+    $Gravedad = (float)  9.807;
+    $Gravedad2 = (string) "9.807 m/s²";
 
-echo $Gravedad;
-echo "<br>";
-echo Gravedad2;
-echo "<hr>";
-echo Gravedad;
-echo "<br>";
-echo $Gravedad2;
+    echo $Gravedad;
+    echo "<br>";
+    echo Gravedad2;
+    echo "<hr>";
+    echo Gravedad;
+    echo "<br>";
+    echo $Gravedad2;
 
-// echo <<<FIN
-// " ' $ \\  ${!${''} = Gravedad}
-// FIN;
-
-
-
+    // echo <<<FIN
+    // " ' $ \\  ${!${''} = Gravedad}
+    // FIN;
 ?>
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+// https://comfenalcocursos2022.000webhostapp.com/Variables_PHP_21_02_2022/api.php
 addEventListener("DOMContentLoaded", async(e)=>{
-    let peticion = await fetch("api.php");
+    let peticion = await fetch("https://comfenalcocursos2022.000webhostapp.com/Variables_PHP_21_02_2022/api.php");
     let texto = await peticion.text();
     document.body.insertAdjacentHTML("afterend", texto);
     // let Gravedad = 9.807;
